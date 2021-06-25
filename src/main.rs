bd2705b9f02ce890e6925148e252c7369b74450c
Guilherme775 <guilhermereis28122002@gmail.com>
2021-06-25 10:40:16 -0300

    chore: change port

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-use std::io;
+use std::{env, io};
 use actix_web::{HttpServer, App};
 use api::routes::{hello, index};
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "3000".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(index)
     })
-    .bind("https://murmuring-caverns-23223.herokuapp.com")?
+    .bind(("0.0.0.0", port))
+    .expect("Can not bind to port 8080")
     .run()
     .await
 }
